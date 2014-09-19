9add103e0a726ef29d20b3e2703e734de660e701
pezholio <pezholio@gmail.com>
2014-09-19 09:20:33 +0100

    Update Certificate to use new API

--- lib/certificate-factory/certificate.rb
+++ lib/certificate-factory/certificate.rb
@@ -6,7 +6,7 @@ module CertificateFactory
     base_uri ENV['BASE_URI']
     basic_auth ENV['ODC_USERNAME'], ENV['ODC_API_KEY']
     headers 'Content-Type' => 'application/json'
-    default_timeout 360
+    default_timeout 120
 
     def initialize(url)
       @url = url
@@ -14,13 +14,41 @@ module CertificateFactory
 
     def generate
       response = post
-      {
-        success: response["success"],
-        published: response["published"],
-        documentation_url: @url,
-        dataset_url: dataset_url(response["dataset_id"]),
-        user: response["owner_email"]
-      }
+      if response["success"] == "pending"
+        @dataset_url = response["dataset_url"]
+        {
+          success: "pending",
+          documentation_url: @url,
+          dataset_url: @dataset_url
+        }
+      else
+        {
+          success: "false",
+          published: "false",
+          documentation_url: @url,
+          error: get_error(response)
+        }
+      end
+    end
+
+    def result
+      if @dataset_url
+        result = get_result(@dataset_url)
+        {
+          success: result["success"],
+          published: result["published"],
+          documentation_url: @url,
+          dataset_url: dataset_url(result["dataset_id"]),
+          user: result["owner_email"]
+        }
+      else
+        response = generate
+        if @dataset_url
+          self.result
+        else
+          response
+        end
+      end
     end
 
     def post
@@ -41,6 +69,26 @@ module CertificateFactory
 
       def dataset_url(id)
         "#{self.class.base_uri}/datasets/#{id}"
+      end
+
+      def get_result(url)
+        result = self.class.get(url)
+        if result["success"] == "pending"
+          sleep 5
+          get_result(url)
+        else
+          result
+        end
+      end
+
+      def get_error(response)
+        if response["errors"]
+          response["errors"].first
+        elsif response.code == 401
+          "Username and / or API key not recognised"
+        else
+          "Unknown error"
+        end
       end
 
   end
--- spec/certificate_spec.rb
+++ spec/certificate_spec.rb
@@ -12,12 +12,54 @@ describe CertificateFactory::Certificate do
     expect(certificate.send(:body)).to eq("{\"jurisdiction\":\"gb\",\"create_user\":\"true\",\"dataset\":{\"documentationUrl\":\"http://data.gov.uk/dataset/defence-infrastructure-organisation-disposals-database-house-of-commons-report\"}}")
   end
 
-  it "generates a certificate" do
-    stub_request(:post, "http://api%40example.com:abc123456@open-data-certificate.dev/datasets")
-                .to_return(body: load_fixture("certificate.json"), :headers => {"Content-Type" => "application/json"})
-
+  it "requests certificate creation", :vcr do
     certificate = CertificateFactory::Certificate.new(@url)
     result = certificate.generate
+
+    expect(result[:success]).to eq("pending")
+    expect(result[:dataset_url]).to match /http:\/\/open-data-certificate.dev\/datasets\/[0-9]+.json/
+  end
+
+  it "returns an error if dataset already exists", :vcr do
+    stub_request(:post, "http://#{ENV['ODC_USERNAME']}:#{ENV['ODC_API_KEY']}@open-data-certificate.dev/datasets")
+          .to_return(body: {success: "false", errors: ["Dataset already exists"]}.to_json,
+                     headers: {content_type: "application/json"})
+
+    certificate = CertificateFactory::Certificate.new(@url)
+
+    result = certificate.generate
+    expect(result[:success]).to eq("false")
+    expect(result[:error]).to eq("Dataset already exists")
+  end
+
+  it "returns an error if the authentication fails" do
+    stub_request(:post, "http://#{ENV['ODC_USERNAME']}:#{ENV['ODC_API_KEY']}@open-data-certificate.dev/datasets")
+          .to_return(body: "", status: 401)
+
+    certificate = CertificateFactory::Certificate.new(@url)
+
+    result = certificate.generate
+    expect(result[:success]).to eq("false")
+    expect(result[:error]).to eq("Username and / or API key not recognised")
+  end
+
+  it "keeps trying for a result until it gets one", :vcr do
+    certificate = CertificateFactory::Certificate.new(@url)
+    dataset_url = "http://open-data-certificate.dev/datasets/1.json"
+
+    allow(CertificateFactory::Certificate).to receive(:get)
+        .and_return({"success" => "pending"}, {"success" => "pending"}, {"success" => "true"})
+
+    result = certificate.send(:get_result, dataset_url)
+
+    expect(CertificateFactory::Certificate).to have_received(:get).exactly(3).times
+    expect(result["success"]).to eq("true")
+  end
+
+  it "requests and gets a certificate", :vcr do
+    certificate = CertificateFactory::Certificate.new(@url)
+
+    result = certificate.result
 
     expect(result[:success]).to eq(true)
     expect(result[:published]).to eq(true)
@@ -25,4 +67,18 @@ describe CertificateFactory::Certificate do
     expect(result[:dataset_url]).to match /http:\/\/open-data-certificate.dev\/datasets\/[0-9]+/
     expect(result[:user]).to eq(ENV['ODC_USERNAME'])
   end
+
+  # it "generates a certificate" do
+  #   stub_request(:post, "http://api%40example.com:abc123456@open-data-certificate.dev/datasets")
+  #               .to_return(body: load_fixture("certificate.json"), :headers => {"Content-Type" => "application/json"})
+  #
+  #   certificate = CertificateFactory::Certificate.new(@url)
+  #   result = certificate.generate
+  #
+  #   expect(result[:success]).to eq(true)
+  #   expect(result[:published]).to eq(true)
+  #   expect(result[:documentation_url]).to eq(@url)
+  #   expect(result[:dataset_url]).to match /http:\/\/open-data-certificate.dev\/datasets\/[0-9]+/
+  #   expect(result[:user]).to eq(ENV['ODC_USERNAME'])
+  # end
 end
